614c0f305197c61e47e228f28b0caa5c97ebfb60
Zack Backmen <backmen@bk.ru>
2023-02-17 19:06:59 +0300

    upd

--- src/AutoGenerator.py
+++ src/AutoGenerator.py
@@ -1,12 +1,13 @@
 import random
-from SpeakerPatch import dynVoices
+from src.SpeakerPatch import dynVoices
 
 example_text = ["Съешь же ещё этих мягких французских булок, да выпей чаю.",
         "Клоун, прекрати разбрасывать банановые кожурки офицерам под ноги!",
         "Капитан, вы уверены что хотите назначить клоуна на должность главы персонала?",
         "Эс Бэ! Тут человек в сером костюме, с тулбоксом и в маске! Помогите!!"]
 def Generate(model,ct):
-    for idx in range(1, ct, 1):
+    for idx in range(1, int(ct), 1):
         model.apply_tts(text=random.choice(example_text), speaker="random", sample_rate=24000, put_accent=False, put_yo=False)
-        model.save_random_voice(dynVoices+"/Voice"+idx+".pt")
+        model.save_random_voice(dynVoices+"/Voice"+str(idx)+".pt")
+        yield dynVoices+"/Voice"+str(idx)+".pt"
 
