5b34a34f46f73e17bf48ac322c481ab0f5a56c00
Zack Backmen <backmen@bk.ru>
2023-02-18 11:57:49 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,11 @@
 import os
 from gevent.pywsgi import WSGIServer
 print("start load app!")
-from ss14tts import app,WarmUp,model,example_text
+from ss14tts import app,WarmUp,model,speakers
 
 print("init!")
 
 http_server = WSGIServer(("0.0.0.0", int(os.environ.get("PORT","5000"))), app)
-WarmUp(model,example_text)
+WarmUp(model,speakers)
 print("run!")
 http_server.serve_forever()
